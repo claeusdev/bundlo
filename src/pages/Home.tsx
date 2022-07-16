ca6e1502a9153fc75256b7ba570bf5d329ae4cb3
Nana Adjei Manu <claeusdev@gmail.com>
2022-07-16 14:35:09 +0200

    feat: render package details to screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,40 @@
-const HomePage = () => {
-  return <h1>Homepage</h1>
-}
+import axios from "axios";
+import React from "react";
 
-export { HomePage }
+const HomePage = () => {
+  const [lib, setLib] = React.useState();
+  const [searchTerm, setTerm] = React.useState<string>("");
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string>("");
+
+  React.useEffect(() => {}, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const res = await axios.get(`http://localhost:3000/search?s=${searchTerm}`);
+    if (res) {
+      setLib(res.data);
+      setIsLoading(false);
+    } else {
+      setError("Package could not be found");
+      setIsLoading(false);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSearch}>
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+      {isLoading ? <h3>Loading ... </h3> : <>{JSON.stringify(lib, null, 2)}</>}
+    </React.Fragment>
+  );
+};
+
+export { HomePage };
